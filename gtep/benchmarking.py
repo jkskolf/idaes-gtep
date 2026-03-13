4043aab03b2aba8ebc5e3d92f80079cf0fa589ec
Kyle Skolfield <jkskolf@sandia.gov>
2026-03-13 11:05:21 -0600

    test on 123

--- gtep/benchmarking.py
+++ gtep/benchmarking.py
@@ -67,7 +67,8 @@ with open(log_folder + "/input.log", "w") as fil:
     dispatch
 
 
-data_path = "./gtep/data/Texas_2000"
+# data_path = "./gtep/data/Texas_2000"
+data_path = "./gtep/data/123_Bus_Resil_Weel"
 # data_path = "./gtep/data/5bus"
 data_object = ExpansionPlanningData()
 data_object.load_prescient(data_path, length_representative_periods)
